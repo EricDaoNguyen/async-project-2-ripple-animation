0a5ad939370be69edd96bd67097c086df31b6aa8
EricDaoNguyen <EricDaoNguyen@Gmail.com>
2021-09-17 16:36:59 -0400

    Added background and orbs.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import * as Three from 'three'
+import { Canvas, useLoader } from 'react-three-fiber'
+import Orb from './assets/customOrb.png'
+import { Suspense, useMemo } from 'react'
+
+// ----------------------------------------------------------------------------------------------------
+
+// Camera position
+function AnimationCanvas() {
+  return(
+    <Canvas
+    colorManagement={false}
+    camera={{position:[100, 10, 0], fov: 90}}
+    >
+      <Suspense fallback={null}>
+        <Points/>
+      </Suspense>
+    </Canvas>
+  )
+}
+
+// ----------------------------------------------------------------------------------------------------
+
+// Contains animation
+function Points() {
+  // Loads orb
+  const orb = useLoader(Three.TextureLoader, Orb)
+
+  // Number of points across an axis
+  const count = 100
+
+  // Distance between each point
+  const distance = 4
+
+  // Creates positions in a 1D array rather than a 2D array with useMemo hook
+  // Example: [x1, y1, z1, x2, y2, z2, x3 and so on...]
+  let positions = useMemo(() => {
+    let positions = []
+
+    // Iterate over an index on x and z axis
+    // From left to right on x
+    // From back to front on z
+    // Determine y on each corresponding point
+    // Add all three into positions array
+    for(let xi = 0; xi < count; xi++) {
+      for(let zi = 0; zi < count; zi++) {
+        let x = distance * (xi - count / 2)
+        let z = distance * (zi - count / 2)
+        let y = 0
+        positions.push(x, y, z)
+      }
+    }
+
+    return new Float32Array(positions)
+  }, [count, distance])
+
+  return(
+    <points>
+      <bufferGeometry attach="geometry">
+        <bufferAttribute
+        attachObject={['attributes', 'position']}
+        array={positions}
+        count={positions.length / 3}
+        itemSize={3}
+        />
+      </bufferGeometry>
+
+      <pointsMaterial
+      attach="material"
+      map={orb}
+      size={0.5}
+      sizeAttenuation
+      transparent={false}
+      alphaTest={0.5}
+      opacity={1.0}
+      />
+    </points>
+  )
+}
+
+// ----------------------------------------------------------------------------------------------------
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense fallback={<div>Loading...</div>}>
+        <AnimationCanvas/>
+      </Suspense>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
