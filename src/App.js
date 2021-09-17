2e8f552876fb0403b9f059e6a67c10d1184ecdd7
EricDaoNguyen <EricDaoNguyen@Gmail.com>
2021-09-17 17:11:35 -0400

    Generated static waves.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css'
 import * as Three from 'three'
 import { Canvas, useLoader } from 'react-three-fiber'
 import Orb from './assets/customOrb.png'
-import { Suspense, useMemo } from 'react'
+import { Suspense, useCallback, useMemo } from 'react'
 
 // ----------------------------------------------------------------------------------------------------
 
@@ -26,12 +26,23 @@ function AnimationCanvas() {
 function Points() {
   // Loads orb
   const orb = useLoader(Three.TextureLoader, Orb)
-
   // Number of points across an axis
   const count = 100
-
   // Distance between each point
   const distance = 4
+
+  // SIN uses a wave pattern
+  // y = sin ( frequency * ( x^2 + z^2 + theta ) ) * amplitude
+  // Frequency changes how many times waves occur
+  // Theta changes how far wave travels before repeating
+  // Amplitude changes height of each wave
+  // http://grapher.mathpix.com/
+  let frequency = 0.002
+  let theta = 0
+  let amplitude = 3
+  const graph = useCallback((x, z) => {
+    return Math.sin(frequency * (x**2 + z**2 + theta)) * amplitude
+  }, [theta, frequency, amplitude])
 
   // Creates positions in a 1D array rather than a 2D array with useMemo hook
   // Example: [x1, y1, z1, x2, y2, z2, x3 and so on...]
@@ -47,13 +58,13 @@ function Points() {
       for(let zi = 0; zi < count; zi++) {
         let x = distance * (xi - count / 2)
         let z = distance * (zi - count / 2)
-        let y = 0
+        let y = graph(x, z)
         positions.push(x, y, z)
       }
     }
 
     return new Float32Array(positions)
-  }, [count, distance])
+  }, [count, distance, graph])
 
   return(
     <points>
